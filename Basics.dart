98c936b9b4b8e0104430904702b84ed518240198
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-25 18:24:54 +0530

    Flutter basics

--- Basics.dart
+++ Basics.dart
@@ -2,6 +2,7 @@
 //(Android, ios, web, desktop) applications using one programming language and codebase
 
 // Difference between native, Hybrid and cross platform development
+
 // Benifits of Flutter 
 // 1. Same Ui and business logic in all platforms
 // 2. Increased time to market speed 
@@ -9,3 +10,12 @@
 // 4. the potential ability to go beyond mobile 
 // 5. custom , animated UI of any complexity available 
 // 6. similar to native app performance 
+// 7. own rendering engine
+// 8. Simple platform specific logic implementation 
+
+// Flutter Architecture 
+// Flutter is all about widgets, we have to build widget tree when building UI 
+// Flutter is a framework that allows us to build UI in a platform independent way
+// UI components and logic both are in single Language DART unlike other platforms 
+
+// create a flutter project - flutter create appName 
