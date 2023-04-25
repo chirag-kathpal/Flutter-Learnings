e3f11011676ec3044be5549c1bcf45e4f8b1110b
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-25 13:15:57 +0530

    First commit

---
+++ Basics.dart
@@ -0,0 +1,11 @@
+// Flutter - A Tool that allows you to build  native cross platform
+//(Android, ios, web, desktop) applications using one programming language and codebase
+
+// Difference between native, Hybrid and cross platform development
+// Benifits of Flutter 
+// 1. Same Ui and business logic in all platforms
+// 2. Increased time to market speed 
+// 3. reduced code development time 
+// 4. the potential ability to go beyond mobile 
+// 5. custom , animated UI of any complexity available 
+// 6. similar to native app performance 
