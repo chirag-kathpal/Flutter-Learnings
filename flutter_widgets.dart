27ce77627a3c672e1f28adc78b3233f28ffbeda5
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-27 18:40:49 +0530

    Widgets

---
+++ flutter_widgets.dart
@@ -0,0 +1,19 @@
+// What does main.dart contains in flutter 
+// No changes in the UI at runtime - use stateless widget
+// if want some changes in the runtime of app - stateful widget 
+
+// Container - it is a widget that combines common paintings, positioning, and sizing of the child widgets
+// also a class to store one or more widgets and position them on the screen according to our needs 
+// similar to a box for storing content 
+
+// Center widget - it aligns the child widget to centre of the screen
+
+// Text widget - display a string of text with single style
+// in text we have - fontsize, color, fontweight, backgroundcolor 
+
+// button widget in flutter - graphical control elements that provides a user to  trigger an event such as taking actions, making choices,
+// and many more types of buttons - text buttons(flat buttons), elevated or raised buttons, outlined button
+
+// how to add image in app - image widget
+
+// Rows and Columns Widgets-  mainaxisalignment - space around, space evenly and space between
