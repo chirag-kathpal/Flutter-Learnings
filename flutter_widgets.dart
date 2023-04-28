7dd70b9e97e8017e4899fcb158b889a50e5289bf
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-28 18:24:36 +0530

    some more widgets

--- flutter_widgets.dart
+++ flutter_widgets.dart
@@ -17,3 +17,23 @@
 // how to add image in app - image widget
 
 // Rows and Columns Widgets-  mainaxisalignment - space around, space evenly and space between
+
+// 28/04/2023 
+// Inkwell widget - it is the material widget
+// responds to the touch action performed by the user 
+// responds when user click on it or tap it 
+// so many getsures like double tap, long press and tap down -  flutter application 4
+
+// scrollview and its types - singlechildscrollview - flutter application 5
+
+// Listview - widget used to display item in a linear manner
+// like zomato swiggy to display a list of resturants
+// it is scrollable widget 
+// ListView.builder
+// ListView.seperated  - it has seperatedbuilder, we can seperate our list element from each other by space , line etc.                       
+//                                     flutter application 6
+// 
+// if the design formation is same and content is different use listview and if design is also different use row and column and use 
+// single child scroll view for scrolling purpose
+//
+// flutter_application_7 -
