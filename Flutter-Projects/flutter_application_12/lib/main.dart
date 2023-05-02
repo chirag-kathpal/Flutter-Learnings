58b7467785fbd22601c7e8f66e3a490c371e8a31
Chirag Kathpal <chiragkath7@gmail.com>
2023-05-02 18:00:46 +0530

    Circle avatar widget

---
+++ Flutter-Projects/flutter_application_12/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: const CircleAvatar(
+            backgroundImage:AssetImage('assets/images/boy.jpg'),
+            backgroundColor: Colors.lightGreen,
+            // radius: 100,
+           // child: Text('Hello',style: TextStyle(color: Colors.green,),),
+          ),
+        ),
+      )
+      );
+  }
+}
