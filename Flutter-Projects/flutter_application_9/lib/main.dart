9a18339d070f4f5617e9d4b1fff032738392ac61
Chirag Kathpal <chiragkath7@gmail.com>
2023-05-02 12:47:30 +0530

    padding and margin

---
+++ Flutter-Projects/flutter_application_9/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        // padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.only(top: 11,bottom: 12,left: 12,right: 12),
+        child: Text("Hello world!",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+      )
+      );
+  }
+}
