36aa9de74bd14eaee25c2448eaad6baaa3a52caa
Chirag Kathpal <chiragkath7@gmail.com>
2023-05-02 18:13:35 +0530

    somme more widgets

--- flutter_widgets.dart
+++ flutter_widgets.dart
@@ -1,6 +1,6 @@
 // What does main.dart contains in flutter 
 // No changes in the UI at runtime - use stateless widget
-// if want some changes in the runtime of app - stateful widget 
+// if want some changes in the runtime of app without restarting - stateful widget 
 
 // Container - it is a widget that combines common paintings, positioning, and sizing of the child widgets
 // also a class to store one or more widgets and position them on the screen according to our needs 
@@ -18,11 +18,11 @@
 
 // Rows and Columns Widgets-  mainaxisalignment - space around, space evenly and space between
 
-// 28/04/2023 
+//------------------------------------------------------------------ 28/04/2023 
 // Inkwell widget - it is the material widget
 // responds to the touch action performed by the user 
 // responds when user click on it or tap it 
-// so many getsures like double tap, long press and tap down -  flutter application 4
+// so many gestures like double tap, long press and tap down -  flutter application 4
 
 // scrollview and its types - singlechildscrollview - flutter application 5
 
@@ -36,4 +36,34 @@
 // if the design formation is same and content is different use listview and if design is also different use row and column and use 
 // single child scroll view for scrolling purpose
 //
+
+// -------------------------------------------------------------------2/05/2023 
 // flutter_application_7 -
+// Add decoration in a container 
+// Boxdecoration - borderradius. border, boxshadow,shape 
+
+// flutter_application_8 -
+// Expanded widget and flex attribute
+
+// flutter_application_9 (ws-cube 42)
+// padding and margin 
+
+// flutter_application_10 (ws-cube 48) 
+// Text input widget - TextField
+// it is an input element which holds the alphanumeric data, such as name, password and address 
+// decoration: inputdecoration (
+// focusedborder, enabledborder,disabledborder 
+// suffixtext , suffixicon, prefixicon
+// )
+// keyboardtype  and controller to get text 
+
+// flutter_application_11 (ws-cube 43)
+// listTile widget
+// Leading, title and subtitle, trailing 
+
+// flutter_application_12 ( ws-cube 44)
+// Circle avatar widget - simply a circle in which we can add bg-color, bg-image or just text
+// usually represents a user with his image or with his initials
+
+// -------------------------------------------------------------------------3/05/2023
+// flutter_application_13
