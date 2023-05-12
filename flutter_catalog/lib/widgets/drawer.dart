994d404cc276c129f460e9f8ae4f1ea25146d114
Chirag Kathpal <chiragkath7@gmail.com>
2023-05-12 11:53:58 +0530

    More  widgets in Project

--- flutter_catalog/lib/widgets/drawer.dart
+++ flutter_catalog/lib/widgets/drawer.dart
@@ -6,21 +6,58 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          DrawerHeader(
-              padding: const EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                  margin: const EdgeInsets.all(0),
-                  decoration: BoxDecoration(color: Colors.amber),
-                  accountName: const Text('Chirag'),
-                  accountEmail: const Text('chiragkath7@gmail.com'),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage:
-                        AssetImage('assets/images/login-image.png'),
-                  ))),
-        ],
+      child: Container(
+        color: Colors.blue.shade300,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          scrollDirection: Axis.vertical,
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                    margin: EdgeInsets.all(0),
+                    decoration: BoxDecoration(color: Colors.blue.shade300),
+                    accountName: Text('Chirag'),
+                    accountEmail: Text('chiragkath7@gmail.com'),
+                    currentAccountPicture: const CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/login-image.png'),
+                    ))),
+            const ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Home',
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.person_pin_circle_sharp,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Profile',
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Contact Us',
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
