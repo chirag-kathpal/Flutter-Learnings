7d5845fe302bd8b6d5fde432de75aac294801cb6
Chirag <chiragkath7@gmail.com>
2023-06-20 13:50:14 +0530

    Flutter Riverpod

--- Riverpod/project_1/lib/main.dart
+++ Riverpod/project_1/lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_1/models/user.dart';
 import 'package:project_1/pages/homepage.dart';
 
+// final nameProvider = Provider<String>((ref) {
+//   return 'Chirag';
+// });
+
+// final nameProvider = StateProvider<String?>((ref) => null);
+// To change the value of provider later on, we use stateProvider
+
+final userProvider = StateNotifierProvider<UserNotifier, User>(
+    (ref) => UserNotifier(const User(name: '', age: 0)));
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +27,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'RiverPod',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
       ),
       home: const MyHomePage(),
     );
   }
 }
+
+// Providers 
+// Provider 
+// StateProvider 
+// StateNotifier and StateNotifierProvider 
+// FutureProvider
--- Riverpod/project_1/lib/pages/homepage.dart
+++ Riverpod/project_1/lib/pages/homepage.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_1/main.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
+  void onSubmit(WidgetRef ref, String value) {
+    // ref.read(nameProvider.notifier).update((state) => value);
+    ref.read(userProvider.notifier).updateName(value);
+  }
+
+  void onSubmitAge(WidgetRef ref, String value) {
+    // ref.read(nameProvider.notifier).update((state) => value);
+    ref.read(userProvider.notifier).updateAge(int.parse(value));
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final name = ref.watch(nameProvider) ??
+    //     ""; // continously keep listening the provider
+    // final nameRead = ref.read(nameProvider); // read only once
+
+    final user = ref.watch(userProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(user.name),
+      ),
+      // extendBodyBehindAppBar: true,
+      body: Column(
+        children: [
+          Center(
+            child: TextField(
+              onSubmitted: (value) => onSubmit(ref, value),
+              decoration: const InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'name',
+              ),
+            ),
+          ),
+          Center(
+            child: TextField(
+              onSubmitted: (value) => onSubmitAge(ref, value),
+              decoration: const InputDecoration(
+                hintText: 'Enter your age',
+                labelText: 'age',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Text(user.age.toString()),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
