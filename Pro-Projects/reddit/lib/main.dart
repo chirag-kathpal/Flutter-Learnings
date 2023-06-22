d6123a95f0319afb617c4b7d6849d1a913c3c461
Chirag <chiragkath7@gmail.com>
2023-06-22 18:50:05 +0530

    login Screen

--- Pro-Projects/reddit/lib/main.dart
+++ Pro-Projects/reddit/lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit/features/auth/screens/login_screen.dart';
+import 'package:reddit/firebase_options.dart';
 import 'package:reddit/theme/pallete.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +26,7 @@ class MyApp extends StatelessWidget {
       title: 'Reddit',
       theme: Pallete.darkModeAppTheme,
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
