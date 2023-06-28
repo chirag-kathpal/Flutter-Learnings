7f79cb1adacc7447778c8325caca54e734912160
Chirag <chiragkath7@gmail.com>
2023-06-28 12:38:56 +0530

    reddit clone deleted

--- Pro-Projects/reddit/lib/core/constants/firebase_constants.dart
+++
@@ -1,6 +0,0 @@
-class FirebaseConstants {
-  static const usersCollection = 'users';
-  static const communitiesCollection = 'communities';
-  static const postscollection = 'posts';
-  static const commentsCollection = 'comments';
-}
--- Pro-Projects/reddit/lib/core/type_defs.dart
+++
@@ -1,5 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:reddit/core/failures.dart';
-
-typedef FutureEither<T> = Future<Either<Failure, T>>;
-typedef FutureVoid = FutureEither<void>;
